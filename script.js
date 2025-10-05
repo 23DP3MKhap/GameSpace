5d3ff83b11e39e300328c784b5d5351a4c390619
misha <misha.hapilov@gmail.com>
2025-10-05 11:34:44 +0300

    modal windows

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ aside.addEventListener("click", function() {
     aside.style.display = "none";
     hideBtn.style.display = "inline-block"; 
 });
+
+
+const modal1 = document.getElementById("modal1");
+const modal2 = document.getElementById("modal2");
+const modal3 = document.getElementById("modal3");
+
+document.getElementById("btn1").onclick = () => modal1.style.display = "flex";
+document.getElementById("btn2").onclick = () => modal2.style.display = "flex";
+document.getElementById("btn3").onclick = () => modal3.style.display = "flex";
+
+
+document.querySelectorAll(".close").forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.closest(".modal").style.display = "none";
+  });
+});
+

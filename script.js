fcabc0ee2ffea256826c0ab797f456185714562c
Mikhail Khapilov <A231064MK@rvt.lv>
2025-10-29 10:39:51 +0200

    contact form w validation added

--- script.js
+++ script.js
@@ -35,3 +35,41 @@ btn.onclick = function() {
     link.setAttribute('href', 'dark.css');}
   else {link.setAttribute('href', 'light.css');}
 };
+
+
+const contactForm = document.getElementById("contact-form");
+const vardsInput = document.getElementById("vards-input");
+const emailInput = document.getElementById("email-input");
+const zinojumsInput = document.getElementById("zinojums-input");
+const atsusitButton = document.getElementById("atsusit-button");
+const contactFormErrors = document.getElementById("contactForms-errors");
+
+contactForm.addEventListener("submit", (submit) => {
+  contactFormErrors.innerHTML = "";
+  const errors = [];
+
+  if (vardsInput.value.length < 2) {
+  errors.push("Vārdam jābūt vismaz 2 simboli");
+  } 
+  
+  else if (!/^[a-zA-Z]+$/.test(vardsInput.value)) {
+  errors.push("Vārda laukā drīkst būt tikai burti");
+  }
+  
+  else if (vardsInput.value.length > 50) {
+  errors.push("Vārds nedrīkst būt garāks par 50 simboliem.");
+  }
+
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value)) {
+    errors.push("Lūdzu, ievadiet derīgu e-pasta adresi");
+  }
+
+  if (zinojumsInput.value.length < 1) {
+    errors.push("Ziņojuma lauks nedrīkst būt tukšs.");
+}
+
+  if (errors.length > 0) {
+    submit.preventDefault();
+    contactFormErrors.innerHTML = errors.map(e => `<div>${e}</div>`).join("");
+  }
+});
